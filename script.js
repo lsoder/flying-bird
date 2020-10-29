ba1b3c602c875975371c7c83d370996de7906947
Linda Söder <lindasoder@Lindas-MacBook-Pro.local>
2020-10-29 10:25:37 +0100

    adds an animated bird that flies to the right

---
+++ script.js
@@ -0,0 +1,31 @@
+
+window.onload = start;
+
+/**
+ * The position of the bird relative to the left side of the page.
+ */
+let left = 0;
+
+/** Starts the program on page load. */
+
+function start() {
+    fly();
+}
+
+
+/** Set an interval to make the bird "fly" */
+function fly() {
+    setInterval(moveBirdToTheRight, 10)
+}
+/** Moves the bird a small step in percentage to the right.  */
+function moveBirdToTheRight() {
+    // Fetch bird from html
+    const bird = document.querySelector('img');
+    
+    // Update position
+    left += 0.2;
+    
+    // Render position
+    bird.style.left = left + '%';
+}
+
